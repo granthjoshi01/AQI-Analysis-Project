da7d1443b17c5fd78ac9dc6301cb862d9da6522b
Granth Joshi <granthjoshi01@gmail.com>
2025-12-12 12:19:24 +0530

    Update aqi.py

--- pipeline/aqi.py
+++ pipeline/aqi.py
@@ -1,6 +1,7 @@
+#enter your API KEY in url
 import requests
 
-url = "https://api.openweathermap.org/data/2.5/air_pollution?lat=0&lon=0&appid=172d73aff141b30936ee738b97488e23"
+url = "https://api.openweathermap.org/data/2.5/air_pollution?lat=0&lon=0&appid=API_KEY"
 
 response = requests.get(url)
 data = response.json()
